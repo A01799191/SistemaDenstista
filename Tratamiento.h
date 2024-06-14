a16320ffd7076417336965722b6f50950260a9c2
A01799191 <A01799191@tec.mx>
2024-06-13 20:47:21 -0600

    Creación de la clase Tratamiento.h
    Implementación de la clase Tratamiento.h nuevo las siguientes sera la implementacion de la clase Fecha y Hora

--- Tratamiento.h
+++ Tratamiento.h
@@ -1,3 +1,27 @@
 
 #include <iostream>
 using namespace std;
+#include <string>
+#include "Fecha.h"
+
+class Tratamiento
+{
+private:
+    string descripcion;
+    Fecha fechaInicio;
+    Fecha fechaFin;
+    string estado;
+public:
+    string getDescripcion();
+    void setDescripcion(string nuevaDescripcion);
+    Fecha getFechaIninico();
+    void setFechaInicio(Fecha nuevaFechaInicio);
+    Fecha getFechaFin();
+    void setFechaFin(Fecha nuevaFechaFin);
+    string getEstado();
+    void setEstado(string nuevoEstado);
+
+    
+};
+
+
